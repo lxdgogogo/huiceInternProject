bdd56ac89ff3ce1893d59f27166675f259595470
SoonMachine <sunamnxin@126.com>
2021-07-19 00:37:48 +0800

    #20210719 create project

---
+++ service-test/src/main/java/com/huice/servicetest/ServiceTestApplication.java
@@ -0,0 +1,13 @@
+package com.huice.servicetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceTestApplication.class, args);
+    }
+
+}
