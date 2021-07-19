0a11e05193c034718f96fade77007add7ac57bfe
GuoDong <2587953647@qq.com>
2021-07-19 11:31:08 +0800

    #719 1130 create service_fdk

---
+++ service_fdk/src/main/java/com/huice/service_fdk/ServiceFdkApplication.java
@@ -0,0 +1,13 @@
+package com.huice.service_fdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceFdkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceFdkApplication.class, args);
+    }
+
+}
---
+++ service_fdk/src/test/java/com/huice/service_fdk/ServiceFdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huice.service_fdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceFdkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
