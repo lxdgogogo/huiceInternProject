df6f352d9a152ec06bb2a1e718b55ba8697c389c
bonnieye <bonnieyqh@163.com>
2021-07-19 14:03:21 +0800

    增加了部分VO，添加了FDK_GOODS2中的表结构

--- service_fdk/src/main/java/com/huice/service_fdk/service/ForwarderSupplierVO.java
+++ service_fdk/src/main/java/com/huice/service_fdk/service/ForwarderSupplierVO.java
@@ -3,10 +3,10 @@ package com.huice.service_fdk.service;
 public class ForwarderSupplierVO {
     private final String business;//档口
     private final String city;//城市
-    private final int cityCode;//城市编码
+    private final Integer cityCode;//城市编码
     private final String floor;//楼层
-    private final int floorCode;//楼层编码
-    private final int forwarderSupplierId;//代拿端档口id
+    private final Integer floorCode;//楼层编码
+    private final Integer forwarderSupplierId;//代拿端档口id
     private final String groupName;//分组名称 (城市+市场)
     private final String market;//市场
     private final String marketCode;//市场编码
--- service_fdk/src/main/java/com/huice/service_fdk/service/SummaryVO.java
+++ service_fdk/src/main/java/com/huice/service_fdk/service/SummaryVO.java
@@ -1,10 +1,16 @@
 package com.huice.service_fdk.service;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
 public class SummaryVO {
     private String avatar;//对方头像URL
-    private String createTime;//创建时间
-    private String lastUpdateTime;//最后更新时间
-    private int saleOrderId;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date createTime;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date lastUpdateTime;
+    private long saleOrderId;
     private String saleOrderNo;//单号
     private String sellerName;//对方名称
     private Number totalAmount;//单内金额总计
