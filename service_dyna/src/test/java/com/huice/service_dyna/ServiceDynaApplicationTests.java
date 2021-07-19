9e6861d2627098eccceb001fdf89bb061a7b2dd6
SoonMachine <sunamnxin@126.com>
2021-07-19 10:57:15 +0800

    #20210719 create service_dyna1

---
+++ service_dyna/src/test/java/com/huice/service_dyna/ServiceDynaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huice.service_dyna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDynaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
