764c0511fd4ccbff271aaa3c0181ec472e17c097
lxdgogogo <1636614463@qq.com>
2021-07-20 19:03:54 +0800

    需求12

--- service_fdk/src/main/java/com/huice/service_fdk/service/market.java
+++ service_fdk/src/main/java/com/huice/service_fdk/service/market.java
@@ -2,10 +2,44 @@ package com.huice.service_fdk.service;
 
 import java.util.ArrayList;
 
-public class market {
-    private int cityCode;//城市编码
-    private ArrayList<floor> floorList;//楼层列表
-    private int marketCode;//市场编码
-    private int marketId;//市场ID
+public class Market {
+    private  long cityCode;//城市编码
+    private ArrayList<Floor> floorList;//楼层列表
+    private final long marketCode;//市场编码
     private String marketName;//市场名称
+
+    public Market(long marketCode) {
+        this.marketCode = marketCode;
+    }
+
+    public ArrayList<Floor> getFloorList() {
+        return floorList;
+    }
+
+    public long getCityCode() {
+        return cityCode;
+    }
+
+    public long getMarketCode() {
+        return marketCode;
+    }
+
+    public String getMarketName() {
+        return marketName;
+    }
+
+    public void setMarketName(String marketName) {
+        this.marketName = marketName;
+    }
+
+    public void setCityCode(long cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public Market(long cityCode, long marketCode, String marketName) {
+        this.cityCode = cityCode;
+        this.marketCode = marketCode;
+        this.marketName = marketName;
+        this.floorList = new ArrayList<>();
+    }
 }
