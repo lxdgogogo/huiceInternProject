a69e5b5fe8bc134f305df5ec9f94d4e4b220d883
bonnieye <bonnieyqh@163.com>
2021-07-21 13:56:37 +0800

    gateway

---
+++ service_fdk/src/main/java/com/huice/service_fdk/dao/SellerDao.java
@@ -0,0 +1,19 @@
+package com.huice.service_fdk.dao;
+
+import com.huice.service_fdk.Controller.vo.SellerSpuVO;
+import com.huice.service_fdk.common.page.PageContent;
+import com.huice.service_fdk.common.page.PageContentContainer;
+import com.huice.service_fdk.common.page.PageParam;
+import com.huice.service_fdk.entity.QueryPageSellerSpuParam;
+import com.huice.service_fdk.service.ForwarderSupplierVO;
+
+public class SellerDao {
+
+    public PageContentContainer<SellerSpuVO> getSellerGoodsQueryPage(QueryPageSellerSpuParam pageParam, Long merchant_id) {
+        PageContent<SellerSpuVO> pageContent = new PageContent<>();
+
+
+
+        return null;
+    }
+}
